ca51014738d3dd1f11b36c608d187c4821d2b1ee
Husniyyah Rogers <husniyyahrogers@gmail.com>
2022-06-10 14:10:28 -0600

    husniyyah

---
+++ SaveItem.test.ts
@@ -0,0 +1,18 @@
+import {Builder, By, Capabilities, WebDriver, until} from "selenium-webdriver";
+
+const chromedriver = require('chromedriver');
+
+const driver: WebDriver = new Builder().withCapabilities(Capabilities.chrome()).build()
+
+test('async await functionality', async () => {
+    await driver.get('https://www.bestbuy.com')
+    await driver.wait(until.elementLocated(By.name('st')))
+    await driver.findElement(By.name('st')).sendKeys("kids night light\n")
+    await driver.findElement(By.xpath('//button[@class="header-search-button"]')).click()
+    await driver.findElement(By.xpath('(//a[@href="/site/lumipets-led-kids-night-light-puppy-lamp-with-remote-white/6478041.p?skuId=6478041"])[1]')).click()
+    await driver.findElement(By.xpath('//button[@class= "save-for-later-save"]')).click()
+    await driver.sleep(4000)
+})
+afterAll(async () => {
+    await driver.quit()
+})
